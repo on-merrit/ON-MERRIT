c79ea4426be194a370aa60aed4e7d9476e1b88c8
Thomas Klebel <t.klebel@hotmail.com>
2021-06-29 07:10:45 +0200

    introduce quality control measures for selecting SDGs

--- WP3/Task3.2/spark/jobs/select_SDG_papers/__init__.py
+++ WP3/Task3.2/spark/jobs/select_SDG_papers/__init__.py
@@ -34,7 +34,7 @@ def analyze(ss, cfg):
         .table(db_name + '.paperfieldsofstudy')
     field_of_study = spark \
         .table(db_name + '.fieldsofstudy') \
-        .select(['fieldofstudyid', 'displayname', 'normalizedname']) \
+        .select(['fieldofstudyid', 'displayname', 'normalizedname', 'level']) \
         .withColumnRenamed('displayname', 'fos_displayname') \
         .withColumnRenamed('normalizedname', 'fos_normalizedname')
     # ref for renaming cols: https://stackoverflow.com/a/36302241/3149349
@@ -58,21 +58,57 @@ def analyze(ss, cfg):
               sdg_fields.fos_id == field_of_study.fieldofstudyid,
               "left")
 
+    # only keep these FOS that are lvl 3 or lower (to avoid false positives)
+    field_of_study_selection = field_of_study_selection \
+        .filter(field_of_study_selection.level <= 3)
+
     sdg_paper_ids = field_of_study_selection \
         .join(paper_field_of_study, ['fieldofstudyid'], how='left')
 
-    sdg_papers = sdg_paper_ids.join(papers_df, ['paperid'], how='left')
+    # select based on threshold
+    # the following query:
+    # SELECT percentile_approx(score, array(0.1, 0.25, 0.5, 0.75, 0.9), 100)
+    # FROM `mag2020`.`paperfieldsofstudy`;
+    # returned this:
+    # [0.2985176993685564,0.3910675398743346,0.44254793982898666,0.503432443560662,0.5730498730791892]
+    # and a specific query for level 3 or lower returned this:
+    # stuff = paper_field_of_study.join(field_of_study, "fieldofstudyid", "left").filter(f.col("level") <= 3)
+    # perc = stuff.agg(f.expr('percentile_approx(score, array(0.1, 0.25, 0.5, 0.75, 0.9), 100)').alias("percentiles"))
+    # perc_new = perc.select(f.explode("percentiles").alias("perc"))
+    #
+    # +----------+
+    # | perc |
+    # +----------+
+    # | 0.32260123 |
+    # | 0.39203507 |
+    # | 0.44323525 |
+    # | 0.49717957 |
+    # | 0.55520207 |
+    # +----------+
+    # the algorithm is therefore not very confident, i.e. values do not range
+    # to 1.0
+    # based on the query result, we take 0.497 as threshold. we have at least 25%
+    # of FOS maps to papers. This is quite a high bar, but should result in a
+    # very reliable result
+    selected_sdg_paper_ids = sdg_paper_ids.filter(sdg_paper_ids.score >= 0.497)
+
+    # get the papers
+    sdg_papers = selected_sdg_paper_ids.join(papers_df, ['paperid'], how='left')
 
     # only keep papers in our window (2006-2020)
     sdg_papers = sdg_papers \
         .filter((sdg_papers.year > 2005) & (sdg_papers.year <= 2020))
+
+    # keep only papers that come either from a journal or a conference
+    sdg_papers = sdg_papers \
+        .filter(sdg_papers.journalid.isNotNull() |
+                sdg_papers.conferenceinstance.isNotNull())
 
     logger.info("Selected all papers.")
 
     # logger.info("Printing the number of papers we found:\n")
     # print out what we sampled
     # sdg_papers.groupby(sdg_papers.fieldofstudyid).count().show()
-
 
     ##  select columns ---------------------------
     # available columns:
