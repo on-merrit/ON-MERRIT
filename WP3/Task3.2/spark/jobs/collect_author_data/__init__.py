a21324a54a6365f2f769b5952b0cf5347a97d480
Thomas Klebel <t.klebel@hotmail.com>
2021-11-30 15:01:30 +0100

    update author data collection

--- WP3/Task3.2/spark/jobs/collect_author_data/__init__.py
+++ WP3/Task3.2/spark/jobs/collect_author_data/__init__.py
@@ -31,7 +31,7 @@ def analyze(ss, cfg):
 
     # read our set of authors
     author_filename = path.join(cfg['hdfs']['onmerrit_dir'],
-                                "sdg_author_paper_affil.csv")
+                                "fos_author_paper_affil.csv")
     sdg_authors = spark.read.csv(author_filename, header=True)
 
     # also get the table of links between authors and papers
@@ -157,7 +157,7 @@ def analyze(ss, cfg):
 
     logger.info('Writing author table to file...')
     out_file = path.join(cfg['hdfs']['onmerrit_dir'],
-                         "sdg_author_data.csv")
+                         "fos_author_data.csv")
     sdg_author_table. \
         write.csv(out_file, mode="overwrite", header=True, sep=",",
                   quoteAll=True)
